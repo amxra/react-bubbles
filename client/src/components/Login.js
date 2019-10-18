c0e1bfa5b3822c222f63532dd0e7daff63f8fe0a
amxra <amiradediran@gmail.com>
2019-10-18 14:00:54 +0100

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,45 @@
-import React from "react";
-
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+import React, {useRef, useState} from "react";
+import Axios from "axios";
+const Login = (props) => {
+ // make a post request to retrieve a token from the api
+ // when you have handled the token, navigate to the BubblePage route
+ const usernameRef = useRef();
+ const passwordRef = useRef();
+ const [loading, setLoading] = useState(false)
+ const submit = () => {
+   setLoading(true);
+   Axios.post('http://localhost:5000/api/login', {
+     username: usernameRef.current.value,
+     password: passwordRef.current.value,
+   })
+   .then(response => {
+     setLoading(false)
+     console.log(response)
+     localStorage.setItem('payload', response.data.payload)
+     props.history.push('/colors')
+   })
+   .catch(error => {
+     setLoading(false);
+console.log(error);
+   })
+ }
+ return (
+   <div>
+     <h1>Welcome to the Bubble App!</h1>
+     <p>Please login here</p>
+     <div>
+       Username
+     < input ref={usernameRef} type="text" />
+     </div>
+     <div>
+       Password
+     < input ref={passwordRef} type="text" />
+     </div>
+     <div>
+       <button onClick={submit}>{loading ? "Loading" : "Submit"}</button>
+     </div>
+   </div>
+ );
 };
-
 export default Login;
+
