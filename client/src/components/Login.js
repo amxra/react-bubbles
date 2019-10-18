a58869dcd6310d1595684c54afa5cf0630c3eee5
amxra <amiradediran@gmail.com>
2019-10-18 14:21:12 +0100

    mvp reached

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,33 +1,57 @@
-import React, {useRef, useState} from "react";
+import React, {useRef} from "react";
 import Axios from "axios";
+import styled from 'styled-components';
+
+const StyledDiv = styled.div `
+width: 70%;
+margin: 0 auto;
+
+.form{
+  border: 1px solid blue;
+  width: 80%;
+  margin: 2rem auto;
+
+  div{
+    margin: 2rem;
+
+    input{
+      margin-left: 1rem;
+    }
+  }
+}
+`
+
+
+
 const Login = (props) => {
  // make a post request to retrieve a token from the api
  // when you have handled the token, navigate to the BubblePage route
  const usernameRef = useRef();
  const passwordRef = useRef();
- const [loading, setLoading] = useState(false)
+
  const submit = () => {
-   setLoading(true);
+   
    Axios.post('http://localhost:5000/api/login', {
      username: usernameRef.current.value,
      password: passwordRef.current.value,
    })
    .then(response => {
-     setLoading(false)
+     
      console.log(response)
      localStorage.setItem('payload', response.data.payload)
      props.history.push('/colors')
    })
    .catch(error => {
-     setLoading(false);
-console.log(error);
+    
+      console.log(error);
    })
  }
  return (
+  <StyledDiv>
    <div>
      <h1>Welcome to the Bubble App!</h1>
-     <p>Please login here</p>
-     <div>
+     <div className = "form">
+       <div>
        Username
      < input ref={usernameRef} type="text" />
      </div>
@@ -36,9 +60,11 @@ console.log(error);
      < input ref={passwordRef} type="text" />
      </div>
      <div>
-       <button onClick={submit}>{loading ? "Loading" : "Submit"}</button>
+       <button onClick={submit}>Submit</button>
      </div>
    </div>
+   </div>
+   </StyledDiv>
  );
 };
 export default Login;
